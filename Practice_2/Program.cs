7b62bf8d25c95f0641e7f9f707b8715227c455f5
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-05 22:27:19 +0400

    update

--- Practice_2/Program.cs
+++ Practice_2/Program.cs
@@ -1,18 +1,12 @@
 ﻿var purchases = new List<int> { 40, 60, 80, 100 };
 
 var account = 0;
-while (purchases.Count > 0) 
+foreach (var purchase in purchases)
 {
-    var purchase = purchases[0];
-    purchases.RemoveAt(0);
-
     account += purchase;
-
     Console.WriteLine($"Обслужен клиент на сумму {purchase}$");
     Console.WriteLine($"Текущий баланс: {account}$");
-
     Console.ReadKey();
-    
     Console.Clear();
 }
 
