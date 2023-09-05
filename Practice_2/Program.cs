9504a2ab42ada217001ad105059bda3692e51c76
Vladimir <vladim.ignatiew2010@yandex.ru>
2023-09-05 18:24:15 +0400

    Initial commit

---
+++ Practice_2/Program.cs
@@ -0,0 +1,20 @@
+﻿var purchases = new List<int> { 40, 60, 80, 100 };
+
+var account = 0;
+while (purchases.Count > 0) 
+{
+    var purchase = purchases[0];
+    purchases.RemoveAt(0);
+
+    account += purchase;
+
+    Console.WriteLine($"Обслужен клиент на сумму {purchase}$");
+    Console.WriteLine($"Текущий баланс: {account}$");
+
+    Console.ReadKey();
+    
+    Console.Clear();
+}
+
+Console.WriteLine("Очередь клиентов обслужена!");
+Console.WriteLine($"Итоговый баланс: {account}$");
